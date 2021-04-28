ea26ca1920cf3e5d53904747bc18aea5e0888d4f
yumip1008 <yumip1008@plateer.com>
2021-04-28 17:50:47 +0900

    Fix ProductService

--- back_lotteondisplay/src/main/java/com/plateer/lotteonDisplay/service/product/ProductService.java
+++ back_lotteondisplay/src/main/java/com/plateer/lotteonDisplay/service/product/ProductService.java
@@ -1,9 +1,9 @@
 package com.plateer.lotteonDisplay.service.product;
 
 import com.plateer.lotteonDisplay.dto.product.*;
-import com.plateer.lotteonDisplay.util.MultiValueMapMapperUtil;
+import com.plateer.lotteonDisplay.util.ObjectMapperUtil;
 import com.plateer.lotteonDisplay.util.WebClientUtil;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
 import reactor.core.publisher.Flux;
@@ -12,47 +12,44 @@ import reactor.core.publisher.Mono;
 import static com.plateer.lotteonDisplay.constants.ProductConstants.*;
 
 @Service
+@RequiredArgsConstructor
 public class ProductService implements IProductService{
 
-    @Autowired
-    private WebClientUtil webClientUtil;
+    private final WebClientUtil webClientUtil;
+
+    private final ObjectMapperUtil objectMapperUtil;
 
     @Override
     public Mono<CatePdList> getAllProudcts(ProductDisplayOption productDisplayOption) {
+        
+        //API 호출 후, ProductListResponse 타입으로 받고, catePdList Mono로 리턴
+        return webClientUtil.getByParamsGetRequest(GET_ALL_PRODUCTS,
+                objectMapperUtil.convertMultiValueMap(productDisplayOption),
+                ProductListResponse.class)
+                .map(e -> e.getCatePdList());
 
-        //WebClient의 queryParams의 paramter에 맞게 Object를 MultiValueMap으로 변환
-        MultiValueMap<String,String> multiValueMap = MultiValueMapMapperUtil.convertFromObject(productDisplayOption);
-
-        //API 호출 후, Mono<ProductListResponse> 타입으로 받기
-        Mono<ProductListResponse> productList =
-                webClientUtil.getByParamsGetRequest(GET_ALL_PRODUCTS, multiValueMap, ProductListResponse.class);
-
-        //필요한 CatePdList 받기
-        Mono<CatePdList> catePdList = productList.map(e -> e.getCatePdList());
-
-        return catePdList;
     }
 
     @Override
     public Mono<ProductDetailDto> getProduct(ProductDetailRequest productDetailRequest) {
-
-        Mono<ProductDetailResponse> response =
-                webClientUtil.getByFluxBodyPostRequest(GET_Product, productDetailRequest, ProductDetailResponse.class);
-
-        Mono<ProductDetailDto> productDetailDto = response.map(e -> e.getData().get(0));
-
-        return productDetailDto;
+        
+        ///API 호출 후, ProductListResponse 타입으로 받고,ProductDeatilDto Mono로 리턴
+        return  webClientUtil.getByFluxBodyPostRequest(GET_Product, 
+                productDetailRequest, 
+                ProductDetailResponse.class)
+                .map(response -> response.getData().get(0));
+        
     }
 
     @Override
     public Flux<ProductDetailDto> getProducts(Flux<ProductDetailRequest> productDetailRequest) {
-
-        Mono<ProductDetailResponse> response =
-                webClientUtil.getByFluxBodyPostRequest(GET_Product, productDetailRequest, ProductDetailRequest.class, ProductDetailResponse.class);
-
-        Flux<ProductDetailDto> productDetailDto = response.map(e -> e.getData())
+        
+        //API 호출 후, ProductListReponse 타입으로 받고, ProductDetailDto Flux로 리턴
+        return webClientUtil.getByFluxBodyPostRequest(GET_Product, 
+                productDetailRequest, 
+                ProductDetailRequest.class, 
+                ProductDetailResponse.class)
+                .map(response -> response.getData())
                 .flatMapMany(Flux::fromIterable);
-
-        return productDetailDto;
     }
 }
